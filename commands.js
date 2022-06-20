9abcc14298af7a90bc9cbb5d3c3a87c1065dce3b
NotRllyRn <75143746+NotRllyRn@users.noreply.github.com>
2022-06-19 17:33:25 -0700

    HHH

--- commands.js
+++ commands.js
@@ -4,15 +4,14 @@ var client
 const commands = {
     tstatus: async () => {
         Offline = !Offline
-
-        console.log(!Offline ? 'online' : 'idle')
-
         client.setting.setCustomStatus({
             status: !Offline ? 'online' : 'idle',
             text: 'Catch me offline and ill give you $10',
             emoji: null,
             expires: null,
         });
+
+        return `Set status to ${!Offline ? 'online' : 'idle'}.`
     },
 }
 module.exports = {
@@ -20,7 +19,7 @@ module.exports = {
         console.log(name)
         if (commands[name]) {
             console.log("found")
-            return commands[name](...args)
+            return await commands[name](...args)
         } else {
             console.log("no")
             return `Command not found`
